c7e22b85c1a1696f21fb8c11ab8139908fdce74d
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-06 12:49:50 +0100

    javascript

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+// const titre=document.querySelector("h1");
+// new Typewriter(titre,{
+//     loop:true
+    // delay:100,
+// })
+
+// .typeString("Savourez et deguster l'art de la pizzeria")
+// .pauseFor(2000)
+// .deleteSpeed(300)
+// .deleteAll()
+
+// .start();
+
+const title=document.querySelector("h1")
+new Typewriter(title, {
+  loop:true
+})
+.typeString("Savourez et déguster l'art de la pizzeria ")
+.pauseFor(2000)
+.deleteAll()
+// .typeString("Le lieu de l'apprentissage idéal")
+.start();
